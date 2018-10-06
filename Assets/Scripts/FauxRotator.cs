3ac581d1938ec0953d88b31a21a538643b47e137
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2018-10-06 02:01:43 +0200

    Basic input

---
+++ Assets/Scripts/FauxRotator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class FauxRotator : MonoBehaviour
+{
+	public Transform planet;
+	public GameObject character;
+	private Animator characterAnimator;
+	public float speed = 5;
+
+	private int lastMovementInput = 0;
+
+	private void Awake()
+	{
+		characterAnimator = character.GetComponentInChildren<Animator>();
+	}
+
+	private void FixedUpdate()
+	{
+		int currentInput = Input.GetAxis("Movement") > 0 ? 1 : Input.GetAxis("Movement") < 0 ? -1 : 0;
+
+		planet.RotateAround(Vector3.zero, Vector3.forward, Time.deltaTime * speed * currentInput);
+
+
+		if(currentInput == lastMovementInput)
+			return;
+		
+		if(currentInput < 0)
+			characterAnimator.SetTrigger("ToFront");
+		else if(currentInput > 0)
+			characterAnimator.SetTrigger("ToBack");
+		else if(!Input.GetButtonDown("Movement"))
+			characterAnimator.SetTrigger("Stop");
+
+		lastMovementInput = currentInput;
+	}
+
+}
